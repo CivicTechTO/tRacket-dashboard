a5705b5e9a2e556076e609ef17600f73679d62bd
Daniel Soukup <dsoukup123@gmail.com>
2024-05-12 11:22:03 -0400

    refactor for multipage app format, add 404 page, add sysyem map page

--- app/app.py
+++ app/app.py
@@ -1,14 +1,12 @@
 """
 Main dash application.
 """
-from dash import Dash
+from dash import Dash, html
+import dash
 import dash_bootstrap_components as dbc
 import configparser
-from src.utils import Logging, dbc_themes_name_to_url, COLUMN
+from src.utils import Logging, dbc_themes_name_to_url
 import os
-from src.data_loading.main import get_locations, create_api
-from src.data_loading_legacy import DataFormatter
-import dash_leaflet as dl
 
 ### Configs & Settings ###
 
@@ -33,54 +31,15 @@ theme_url = dbc_themes_name_to_url[theme_name]
 
 ### Setup App ###
 
-
 app = Dash(
     "Noise-App",
     title="Noise Pressure Monitor",
     external_stylesheets=[theme_url, dbc.icons.FONT_AWESOME],
+    use_pages=True
 )
 server = app.server
 
-### Data loading ###
-
-api = create_api()
-locations = get_locations(api)
-
-# map string col names to enum
-dataformatter = DataFormatter()
-locations = dataformatter._string_col_names_to_enum(locations)
-
-### Mapping ###
-
-# get map center
-lat = float(config["constants"]["map_center_lat"])
-lon = float(config["constants"]["map_center_lon"])
-center = [lat, lon]
-
-markers = [
-    dl.CircleMarker(
-        center=[lat, lon],
-        radius=config["map"]["radius-pixel"],
-        fillColor=config["map"]["marker_color"],
-        color=config["map"]["marker_color"]
-    )
-    for lat, lon in zip(locations[COLUMN.LAT], locations[COLUMN.LON])
-]
-
-app.layout = dl.Map(
-    [
-        dl.TileLayer(
-            url=config["map"]["layer_url"],
-            attribution=config["map"]["layer_attribution"]
-            ), 
-        dl.LayerGroup(markers),
-        dl.GestureHandling()
-    ],
-    center=center,
-    zoom=config["map"]["zoom"],
-    style={"height": "100vh"},
-)
-
+app.layout = html.Div([dash.page_container])
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=PORT)
---
+++ app/pages/not_found_404.py
@@ -0,0 +1,6 @@
+import dash
+from dash import html
+
+dash.register_page(__name__)
+
+layout = html.H1("Looks like you are looking for something that does not exist 🤔")
